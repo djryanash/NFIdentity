29c5572da9ef7ed5ecfcfe4544b9f04760d0fce1
Ryan Ashton <djryanash@me.com>
2025-01-27 11:23:53 +0800

    First commit.

--- Sources/NFIdentity/NFIdentity.swift
+++ Sources/NFIdentity/NFIdentity.swift
@@ -1,2 +1,20 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public final class Identity {
+    
+    public func encrypt() -> String {
+        "Your data has just been encrypted."
+    }
+    
+    public func decrypt() -> String {
+        "Your data has just been decrypted."
+    }
+    
+    public func viewData() -> String {
+        "You are viewing your data."
+    }
+    
+}
